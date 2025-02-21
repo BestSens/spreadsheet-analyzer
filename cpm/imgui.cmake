f32ddae5b0f43e67eb6e64f4b9990fad7bd81ae1
Jan Schöppach <jan.schoeppach@bestsens.de>
2025-02-21 10:49:30 +0100

    Use docking branch of imgui

--- cpm/imgui.cmake
+++ cpm/imgui.cmake
@@ -2,8 +2,8 @@ include(CPM)
 CPMAddPackage(
   NAME imgui_external
   VERSION 1.91.8
-  URL https://github.com/ocornut/imgui/archive/refs/tags/v1.91.8.zip
-  URL_HASH MD5=19e5a401daa83cfec8016f16fe18e16c
+  URL https://github.com/ocornut/imgui/archive/refs/tags/v1.91.8-docking.zip
+  URL_HASH MD5=43d165df3b3cbea8717873ba213d0c28
   OVERRIDE_FIND_PACKAGE
 )
 
