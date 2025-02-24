4b5314ad06b6565842f8c5735031dde14beb8970
Jan Schöppach <jan.schoeppach@bestsens.de>
2025-02-25 00:10:23 +0100

    Fix static building

--- cpm/sdl-image.cmake
+++ cpm/sdl-image.cmake
@@ -6,8 +6,6 @@ CPMAddPackage(
   URL https://github.com/libsdl-org/SDL_image/archive/refs/tags/release-3.2.0.zip
   URL_HASH MD5=f1a86bb4224972c6e3d6ca18f6cdf99c
   OVERRIDE_FIND_PACKAGE
-  OPTIONS
-    "BUILD_SHARED_LIBS OFF"
 )
 
 FetchContent_MakeAvailable(sdl_image)
