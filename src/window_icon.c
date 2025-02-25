be812e17d1ff8b6bea216934f2e9bef03ebcde66
Jan Schöppach <jan.schoeppach@bestsens.de>
2025-02-25 12:13:04 +0100

    Add new icon

--- src/window_icon.c
+++ src/window_icon.c
@@ -1,7 +1,7 @@
 #include <stddef.h>
 
 const unsigned char icon_data[] = {
-#embed "../assets/app_icon.png"
+#embed "../assets/2025_02_ICON_shade.png"
 };
 
 const size_t icon_data_size = sizeof(icon_data);
