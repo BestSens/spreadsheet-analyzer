00162e0f6e20f1e73285ce49128458a083948798
Jan Schöppach <jan.schoeppach@bestsens.de>
2025-02-27 12:21:29 +0100

    Update spdlog to v1.15.1

--- cpm/spdlog.cmake
+++ cpm/spdlog.cmake
@@ -1,11 +1,11 @@
 include(CPM)
 CPMAddPackage(
   NAME spdlog
-  VERSION 1.15.0
+  VERSION 1.15.1
   OPTIONS
     "SPDLOG_FMT_EXTERNAL ON"
-  URL https://github.com/gabime/spdlog/archive/refs/tags/v1.15.0.tar.gz
-  URL_HASH MD5=822878647de9eacfe43695a674fb551f
+  URL https://github.com/gabime/spdlog/archive/refs/tags/v1.15.1.tar.gz
+  URL_HASH MD5=3a8f758489a1bf21403eabf49e78c3a4
   OVERRIDE_FIND_PACKAGE
 )
 
