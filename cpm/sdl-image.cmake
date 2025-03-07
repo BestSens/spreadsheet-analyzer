e99f285a6bca729e2c8e868162481db8b0a62dca
Jan Schöppach <jan.schoeppach@bestsens.de>
2025-03-07 09:11:32 +0100

    Disable building of shared libs for sdl-image to fix build on Linux

--- cpm/sdl-image.cmake
+++ cpm/sdl-image.cmake
@@ -5,6 +5,8 @@ CPMAddPackage(
   VERSION 3.2.0
   URL https://github.com/libsdl-org/SDL_image/archive/refs/tags/release-3.2.0.zip
   URL_HASH MD5=f1a86bb4224972c6e3d6ca18f6cdf99c
+  OPTIONS
+    "BUILD_SHARED_LIBS OFF"
 )
 
 if (sdl_image_ADDED)
