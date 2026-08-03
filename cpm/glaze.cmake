55f0b12901c3612a2c78782a4fc314a8d75b9988
Jan Schöppach <github@spiderpig.de>
2026-08-03 19:26:27 +0200

    build: update glaze to v8.0.0

--- cpm/glaze.cmake
+++ cpm/glaze.cmake
@@ -1,9 +1,9 @@
 include(CPM)
 CPMAddPackage(
   NAME glaze
-  VERSION 7.9.1
-  URL https://github.com/stephenberry/glaze/archive/refs/tags/v7.9.1.zip
-  URL_HASH SHA256=3014214abb2518ffd843e4396d783083812acf993b3bca6f5df78e676d74dd52
+  VERSION 8.0.0
+  URL https://github.com/stephenberry/glaze/archive/refs/tags/v8.0.0.zip
+  URL_HASH SHA256=772a8e9ea9e9a9e4eeb995c0bdbcbbc7e37faea5f6942bbd7b8d74b029963029
   OPTIONS
     "glaze_DEVELOPER_MODE OFF"
     "glaze_BUILD_EXAMPLES OFF"
